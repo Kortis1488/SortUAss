139db1e747af7d762429a857bd60600ca416fa23
Kortis <kortis1703@gmail.com>
2023-12-05 18:23:12 +0300

    add, but not linked fileBoostSystem

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
-#include "Node.h"
+#include <boost/filesystem.hpp>
+
 using namespace std;
 
 
 
 int main(){
-    Node node;
-    string command;
-    string s = "f1";
-    for(;;){
-        cin >> command;
-        if(command == "exit") break;
-        if(command == "add") {
-            node.addFile(s);
-            s+="1";
-        } 
-        if(command == "show") node.printFiles(); 
-    }
+    
+    string s;
+
     return 0;
 }
